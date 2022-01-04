0375d157f6b33f3f271683021f63fda931c4c485
KatyaDremliuh <nerona.91@gmail.com>
2022-01-04 16:43:12 +0300

    Add servert Get and Post msg

--- ASPCoreServer/Controllers/Messenger.cs
+++ ASPCoreServer/Controllers/Messenger.cs
@@ -1,8 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using System;
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using ConsoleClient;
+using Newtonsoft.Json;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -12,17 +12,38 @@ namespace ASPCoreServer.Controllers
     [ApiController]
     public class Messenger : ControllerBase
     {
+        static List<Message> ListOfMessages = new List<Message>();
         // GET api/<Messenger>/5
         [HttpGet("{id}")]
         public string Get(int id)
         {
-            return "Smth.'s goin' wrong... " + id.ToString();
+            string outPutString = "Not found"; // по умолчанию
+            
+            // а если сообщение валидно, вернем номер смс и его текст 
+            if (id<ListOfMessages.Count && id>=0)
+            {
+                outPutString=JsonConvert.SerializeObject(ListOfMessages[id]);
+            }
+
+            Console.WriteLine($"Запрошено сообщение № {id} : {outPutString}");
+
+            return outPutString;
         }
 
         // POST api/<Messenger>
         [HttpPost]
-        public void Post([FromBody] string value)
+        public IActionResult SendMessage([FromBody] Message msg)
         {
+            if (msg == null)
+            {
+                return BadRequest();
+            }
+
+            ListOfMessages.Add(msg);
+
+            Console.WriteLine($"Всего сообщений: {ListOfMessages.Count}. Посланное сообщение:{msg}");
+            // return new NoContentResult();
+            return new  OkResult();
         }
     }
 }
